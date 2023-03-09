9677505ae4b105db88866c7cf739fe6bb868ea9e
ulises avila camargo <ulisesavila04@gmail.com>
2023-03-09 00:28:38 -0600

    add de basic layout of the page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,26 +1,28 @@
 import { gql, useMutation } from '@apollo/client';
 
+import Title from '@/components/Title';
+
+import ParseString from '@/graphql/ParseString';
+
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-
-import Title from '@/components/Title';
-import { useEffect } from 'react';
-import ParseString from '@/graphql/ParseString';
+import './index.css';
 
 export default function Home() {
   const [parseString, { data, loading, error }] = useMutation(ParseString);
   parseString({ variables: { unparsedString: 'Robert000Smith000123”' } });
-  useEffect(() => {}, []);
+
   return (
-    <main>
-      <Title>Hello TypeScript!</Title>
-      <p>A TypeScript starter for Gatsby. Great for advanced users.</p>
-      <p>
-        Follow me on Twitter (
-        <a href="https://twitter.com/jpedroschmitz">@jpedroschmitz</a>)
-      </p>
-    </main>
+    <>
+      <header>
+        <div className="top-bar">name</div>
+      </header>
+
+      <main className="main">
+        <div>aergh</div>
+      </main>
+    </>
   );
 }
