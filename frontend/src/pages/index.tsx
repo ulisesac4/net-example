c72ad02ef3f62bff6a48d2fb0e2eb20623593407
ulises avila camargo <ulisesavila04@gmail.com>
2023-03-09 01:32:13 -0600

    add more components to the page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,27 +1,53 @@
 import { gql, useMutation } from '@apollo/client';
 
-import Title from '@/components/Title';
-
 import ParseString from '@/graphql/ParseString';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import './index.css';
+import { useState } from 'react';
 
 export default function Home() {
+  const [unparsedString, setUnparsedString] = useState('');
   const [parseString, { data, loading, error }] = useMutation(ParseString);
-  parseString({ variables: { unparsedString: 'Robert000Smith000123”' } });
 
   return (
     <>
       <header>
-        <div className="top-bar">name</div>
+        <div className="top-bar">Tech Challenge</div>
       </header>
 
       <main className="main">
-        <div>aergh</div>
+        <div style={{ textAlign: 'center' }}>
+          <h1>Challenge about decoding a string</h1>
+          <h2>with the following form: "Robert000Smith000123".</h2>
+        </div>
+        <div className="main-content">
+          <TextField
+            id="outlined-basic"
+            label="Outlined"
+            variant="outlined"
+            style={{ height: '48px' }}
+            value={unparsedString}
+            onChange={(event) => {
+              setUnparsedString(event.target.value);
+            }}
+          />
+          <Button
+            variant="contained"
+            color="success"
+            style={{ height: '55px' }}
+            onClick={() => {
+              parseString({ variables: { unparsedString } });
+            }}
+          >
+            Decode
+          </Button>
+        </div>
       </main>
     </>
   );
